2c05b39eb4a3f209dfff122ce9ae26d1c2ab0aa2
Drew Hutton <drew.business2308@gmail.com>
2020-12-08 16:49:44 +1030

    feat: upload data to bucket in production, cleanup, version bump

--- src/wiki/character/list.ts
+++ src/wiki/character/list.ts
@@ -13,54 +13,28 @@ interface CharacterTableData {
 }
 
 const list = async (): Promise<CharacterTableData[]> => {
-  let $ = await fetch(`${wikiBaseURI}/wiki/Characters/List#`);
+  const $ = await fetch(`${wikiBaseURI}/wiki/Characters/List#`);
 
-  if (!$) {
-    return [];
-  }
+  const charTableData: CharacterTableData[] = $('.article-table tbody tr')
+    .toArray()
+    .filter(char => $(char).find('td:nth-child(3) a').text())
+    .map(char => {
+      let link = $(char).find('td:nth-child(3) a').attr('href');
+      link = link ? `${wikiBaseURI}${link}` : '';
 
-  let rows = $('.article-table tbody tr').toArray();
-
-  const charTableData: CharacterTableData[] = [];
-
-  rows.forEach((char, i) => {
-    const name = $(char).find('td:nth-child(3) a').text();
-    if (!name) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      rows[i] = null;
-      return;
-    }
-
-    let link = $(char).find('td:nth-child(3) a').attr('href');
-    link = link ? `${wikiBaseURI}${link}` : '';
-
-    charTableData.push({
-      rarity: Number($(char).find('td:nth-child(1)').text()),
-      image: $(char).find('td:nth-child(2) a').attr('href'),
-      name: name,
-      element: $(char).find('td:nth-child(4) a:last-of-type').text(),
-      weapon: $(char).find('td:nth-child(5) a:last-of-type').text(),
-      sex: $(char).find('td:nth-child(6)').text(),
-      nation: $(char).find('td:nth-child(7) a:first-of-type').text(),
-      link: link,
+      return {
+        rarity: Number($(char).find('td:nth-child(1)').text()),
+        image: $(char).find('td:nth-child(2) a').attr('href'),
+        name: $(char).find('td:nth-child(3) a').text(),
+        element: $(char).find('td:nth-child(4) a:last-of-type').text(),
+        weapon: $(char).find('td:nth-child(5) a:last-of-type').text(),
+        sex: $(char).find('td:nth-child(6)').text(),
+        nation: $(char).find('td:nth-child(7) a:first-of-type').text(),
+        link: link,
+      };
     });
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    rows[i] = null; // Remove Element from memory
-  });
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  $ = null; // Clear root from memory
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  rows = null;
-
-  const copied = JSON.parse(JSON.stringify(charTableData));
-
-  return copied;
+  return charTableData;
 };
 
 export default list;
