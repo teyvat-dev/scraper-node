32e267b0e64881b2970c0d0fb3d4ca641fe00661
Drew Hutton <drew.business2308@gmail.com>
2020-12-20 11:33:23 +1030

    chore: prod ready regions

--- src/wiki/region/handle.ts
+++ src/wiki/region/handle.ts
@@ -1,9 +1,9 @@
 import type { Response } from 'express';
 import type { Storage } from '@google-cloud/storage';
-import md5 from 'crypto-md5';
 
 import scrape from './scrape';
 import { __DEV__ } from '../../constants';
+import checkIsTainted from '../../helpers/checkIsTainted';
 
 const handle = async (res: Response, storage: Storage) => {
   const regions = await scrape();
@@ -18,37 +18,34 @@ const handle = async (res: Response, storage: Storage) => {
 
   const bucket = storage.bucket(process.env.SCRAPER_BUCKET);
 
-  // TODO this needs to be updated to support the ouput of regions
-  // use `wiki_regions/<region name>` as the file name
+  await Promise.all(
+    regions.map(async region => {
+      const data = JSON.stringify(region); // TODO change this all to gzip?
 
-  // await Promise.all(
-  //   characters.map(async character => {
-  //     const data = JSON.stringify(character); // TODO change this all to gzip?
+      const file = bucket.file(`wiki_regions/${region.name}`);
 
-  //     const file = bucket.file(`wiki_characters/${character.character.name}`);
+      const isTainted = await checkIsTainted(file, data);
 
-  //     const metadata = await file.getMetadata();
+      if (!isTainted) {
+        return;
+      }
 
-  //     if (metadata[0].md5Hash === md5(data, 'base64')) {
-  //       return;
-  //     }
+      const fileStream = file.createWriteStream({
+        resumable: false,
+        contentType: 'application/json',
+      });
 
-  //     const fileStream = file.createWriteStream({
-  //       resumable: false,
-  //       contentType: 'application/json',
-  //     });
+      fileStream.on('error', err => {
+        return res.status(500).send({ error: err });
+      });
 
-  //     fileStream.on('error', err => {
-  //       return res.status(500).send({ error: err });
-  //     });
+      fileStream.on('finish', () => {
+        return;
+      });
 
-  //     fileStream.on('finish', () => {
-  //       return;
-  //     });
-
-  //     fileStream.end(data);
-  //   })
-  // );
+      fileStream.end(data);
+    })
+  );
 
   return res.send({ status: 'Done' });
 };
