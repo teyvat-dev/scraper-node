781c1e81e3dec3a4ff0a2f8fd5fde4c14708584b
kirandesimone <kirandesimone@gmail.com>
2020-12-29 18:30:07 -0500

    feat: init element scraper

--- src/wiki/element/types.ts
+++ src/wiki/element/types.ts
@@ -4,3 +4,10 @@ export type ElementOutput = Create.Element;
 
 // Temporary Scraper Data Interfaces
 // TODO: Add new types here
+
+export interface ElementTableData {
+  element: string;
+  statusEffect: string;
+  reactionFormulas: string[];
+  reactionDescriptions: string[];
+}
